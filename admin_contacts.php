988a9925ff1407fd9b95a4a39071f983b4eea57c
K1roo <96072937+K1roo@users.noreply.github.com>
2023-03-15 14:24:55 +0100

    feat(cleanup): Sanitization code

--- admin_contacts.php
+++ admin_contacts.php
@@ -26,10 +26,9 @@ if(isset($_GET['delete'])){
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Messages</title>
 
-   <!-- font awesome cdn link  -->
+   <!-- cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom admin css file link  -->
    <link rel="stylesheet" href="css/admin_style.css">
 
 </head>
@@ -68,13 +67,6 @@ if(isset($_GET['delete'])){
 
 
 
-
-
-
-
-
-
-<!-- js file link  -->
 <script src="js/admin_script.js"></script>
 
 </body>
--- footer.php
+++ footer.php
@@ -50,5 +50,4 @@
 
 </section>
 
-
 <!-- footer section ends -->
