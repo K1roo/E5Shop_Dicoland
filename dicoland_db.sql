e1cdd1ae447e8937bb9cec13a22645bbe1088dbd
K1roo <96072937+K1roo@users.noreply.github.com>
2023-03-15 15:47:17 +0100

    feat(DB): ajoute d'info dans la BDD

--- dicoland_db.sql
+++ dicoland_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mer. 15 mars 2023 à 13:04
+-- Généré le : mer. 15 mars 2023 à 14:46
 -- Version du serveur :  5.7.11
 -- Version de PHP : 8.0.3
 
@@ -71,6 +71,13 @@ CREATE TABLE `orders` (
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(11, 1, 'kirolos_user', '0148595431', 'kirolos@user.com', 'cash on delivery', 'appartement 69 , Rue de Turbigo, Paris, France - 75003', ', livre 2 (1) , livre 1  (1) ', 90, '15-Mar-2023', 'pending');
+
 -- --------------------------------------------------------
 
 --
@@ -83,6 +90,17 @@ CREATE TABLE `products` (
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
+(1, 'livre 1 ', 30, 'book-4.png'),
+(2, 'livre 2', 60, 'be_well_bee.jpg'),
+(3, 'livre 3 ', 15, 'the_girl_of_ink_and_stars.jpg'),
+(4, 'livre 4', 5, 'clever_lands.jpg'),
+(5, 'livre 5', 18, 'book-2.png');
 
 -- --------------------------------------------------------
 
@@ -104,7 +122,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'kirolos_user', 'kirolos@user.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user'),
-(2, 'kirolos_admin', 'kirolos@admin.com', '81dc9bdb52d04dc20036dbd8313ed055', 'admin');
+(2, 'kirolos_admin', 'kirolos@admin.com', '81dc9bdb52d04dc20036dbd8313ed055', 'admin'),
+(3, 'kirolos_user2', 'kirolos@user2.com', '81dc9bdb52d04dc20036dbd8313ed055', 'user');
 
 --
 -- Index pour les tables déchargées
@@ -148,7 +167,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT pour la table `message`
@@ -160,19 +179,19 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
