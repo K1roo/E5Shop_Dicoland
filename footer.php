2c632f58128999e1067cb41834f8c108a49a26ab
K1roo <96072937+K1roo@users.noreply.github.com>
2023-03-17 01:28:09 +0100

    feat(footer): code cleanup

--- footer.php
+++ footer.php
@@ -38,13 +38,6 @@
 
 <!-- Lien sociaux -->
 
-    <div class="share">
-        <a href="#" class="fab fa-facebook-f"></a>
-        <a href="#" class="fab fa-twitter"></a>
-        <a href="#" class="fab fa-instagram"></a>
-        <a href="#" class="fab fa-linkedin"></a>
-        <a href="#" class="fab fa-pinterest"></a>
-    </div>
 
    
 
