0329741944a5fe37384523175d4e63518f4dfda3
K1roo <96072937+K1roo@users.noreply.github.com>
2023-03-22 17:47:30 +0100

    feat(Fix): U.orders are displayed ONLY if they are pending

--- user_orders.php
+++ user_orders.php
@@ -44,7 +44,7 @@ if(isset($_GET['delete'])){
 
    <div class="box-container">
       <?php
-      $select_orders = mysqli_query($conn, "SELECT * FROM `orders` where user_id = $user_id") or die('query failed');
+      $select_orders = mysqli_query($conn, "SELECT * FROM `orders` where user_id = $user_id AND payment_status ='PENDING'") or die('query failed');
       if(mysqli_num_rows($select_orders) > 0){
          while($fetch_orders = mysqli_fetch_assoc($select_orders)){
       ?>
